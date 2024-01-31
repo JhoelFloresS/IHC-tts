feea6c444575ffd358a0a34ab0ed144af845d0a0
Jhoel Flores <jhoel.flores@jala.university>
2024-01-31 19:09:05 -0400

    first

---
+++ lib/consts.dart
@@ -0,0 +1,2 @@
+String TTS_INPUT =
+    "Cada texto incluye ejercicios con preguntas para evaluar la comprensión lograda. Esto te permitirá reforzar tu vocabulario, mejorar la gramática y reconocer expresiones propias del español. Nuestros textos son gratis y están diseñados por nivel de complejidad. Además pueden descargarse en formato PDF.";
